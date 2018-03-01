4847118d311f3b798c84cdcf27a48af04042c64d
Rod Johnson <johnsonroda@gmail.com>
2018-03-01 14:55:09 -0600

    Initial commit from Atomist

---
+++ src/main/java/atomist/Stl5Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stl5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stl5Application.class, args);
+	}
+}
